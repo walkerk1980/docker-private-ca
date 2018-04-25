721a615c5fa7e6932550e52f05783bcb6de67c35
Keith Walker <walkerk1980@gmail.com>
2018-04-25 00:14:44 +0000

    still bugfix diff between local docker and docker hub

--- createCA.sh
+++ createCA.sh
@@ -2,7 +2,8 @@
 cd /root/ca/
 
 /bin/echo -e "\n\rCreating Private Key using pasword specified in ENV VAR PASSWORD...\n\r"
-/bin/echo -e "$PASSWORD\n\r" | openssl genrsa -aes256 -out private/root_private_key.pem -passout stdin && chmod 400 private/root_private_key.pem
+/bin/echo -e "$PASSWORD\n\r" | openssl genrsa -aes256 -out private/root_private_key.pem -passout stdin 
+/bin/chmod 400 private/root_private_key.pem
 /bin/echo -e "\n\rCreating CA Certificate with subject using domain specified in ENV VAR DOMAIN...\n\r"
 /bin/echo -e "$PASSWORD\n\r" | openssl req -config openssl_root.cnf -key private/root_private_key.pem -passin stdin -new -x509 -days 365 -sha256 -extensions v3_ca -out certs/ca_cert.pem -subj "/C=US/ST=Washington/L=Seattle/O=ExampleCompany/CN=$DOMAIN"
 
