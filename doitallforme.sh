74c8f47023031ad4addc188bb592b0735ddf50af
Keith Walker <walkerk1980@users.noreply.github.com>
2018-04-30 14:00:40 -0700

    Update doitallforme.sh
    Add notification about sleeping 10 seconds

--- doitallforme.sh
+++ doitallforme.sh
@@ -5,6 +5,7 @@ echo '' >ca/index.txt
 /usr/local/bin/createCA.sh
 
 /usr/local/bin/createPCA.sh
+/bin/echo -e "\n\rSleeping 10 seconds to allow PCA creation to finish\n\r"
 sleep 10
 /usr/local/bin/signPCA.sh
 
